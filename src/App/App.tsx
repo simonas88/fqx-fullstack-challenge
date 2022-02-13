7b71e5f30aa31506e904497865f56980097a5e24
Simonas Drungilas <s.drungilas@gmail.com>
2022-02-13 18:05:59 +0200

    clean up remnants

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,25 +1,10 @@
-import logo from "./logo.svg";
+import { FC } from "react";
 import "./App.css";
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
-}
+const App: FC = () => (
+	<div className="App" data-testid="app-root">
+
+	</div>
+);
 
 export default App;
